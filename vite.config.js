40379a7c978ce3333293cef42aaff40a8ec9b492
Alan-Yehoshua <111207008+Alan-Yehoshua@users.noreply.github.com>
2024-11-28 06:47:47 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://backendchat-hi9n.onrender.com', // Reemplaza con tu URL de backend en Render
+        changeOrigin: true,
+        secure: true,
+      },
+      '/get_messages': {
+        target: 'https://backendchat-hi9n.onrender.com', // Asegúrate de que todas las rutas que usas apunten al backend en Render
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
