c7f4e47ffb13836e878e4a1ba2d03bf329a0e965
Alan-Yehoshua <Allen_west@outlook.com>
2024-11-27 20:56:29 -0600

    AAAA

--- src/components/Pages/HomePageFreelancer/CreateProfilePageF.jsx
+++ src/components/Pages/HomePageFreelancer/CreateProfilePageF.jsx
@@ -6,6 +6,7 @@ import {
     CardContent,
     CardFooter,
   } from "@/components/ui/card";
+  
   import { Label } from "@/components/ui/label";
   import { Input } from "@/components/ui/input";
   import { Textarea } from "@/components/ui/textarea";
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
