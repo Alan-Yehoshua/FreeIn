cb9bf93fe99f850a08ade7ccef61fdfb157cf0d6
Alan-Yehoshua <111207008+Alan-Yehoshua@users.noreply.github.com>
2024-12-04 08:03:08 -0600

    Update Client.js

--- src/Supabase-BD/Client.js
+++ src/Supabase-BD/Client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://unvdzinvsutaijwtcszz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVudmR6aW52c3V0YWlqd3Rjc3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MDI3NDIsImV4cCI6MjA0MzI3ODc0Mn0.FxeeBmYRANGJJpXzKoPR9xWcB4ozARj8H5VHqkg9_AE"
+  LINK,
+  API_KEY
 );
